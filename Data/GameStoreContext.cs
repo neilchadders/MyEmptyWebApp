8582b2210510cfa09b44f89c8b91805e444abab6
Neil Chadwick <neilchadders@hotmail.com>
2025-04-18 11:46:23 +0100

    add migration

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MyEmptyWebApp.Entities;
 
 namespace MyEmptyWebApp.Data;
 
--- Entities/Game.cs
+++ Entities/Game.cs
@@ -5,12 +5,13 @@ namespace MyEmptyWebApp.Entities;
 public class Game
 {
     public int Id {get; set; }
-    public required string Title { get; set; }
+    public required string Name { get; set; }
 
     public int GenreId { get; set; }
 
     public Genre? Genre { get; set; } // Navigation property to Genre
 
+    public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set;}
 }
 
